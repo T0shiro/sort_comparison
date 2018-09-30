b920fe69c3087e8ae380d9c1ee7e09beeec99a2b
Johann Mortara <johann.mortara@etu.unice.fr>
2018-09-30 13:42:47 +0200

    Added first, random and median-of-3 pivot choices.

--- algorithms/quick_sort.groovy
+++ algorithms/quick_sort.groovy
@@ -2,36 +2,58 @@ package algorithms
 
 class quick_sort {
 
+    private Random random = new Random()
+
     def run(List<Integer> values) {
-        quicksort(values, 0, values.size() - 1)
+        quicksort(values, 0, values.size() - 1, this.&median_random)
         return values
     }
 
-    private def quicksort(List<Integer> values, int start, int end) {
+    private def quicksort(List<Integer> values, int start, int end, Closure median) {
         if (start < end) {
-            def pivot = partition(values, start, end)
-            quicksort(values, start, pivot - 1)
-            quicksort(values, pivot + 1, end)
+            def p = partition(values, start, end, median)
+            quicksort(values, start, p, median)
+            quicksort(values, p + 1, end, median)
         }
     }
 
-    private def partition(List<Integer> values, int start, int end) {
-        def pivot = values[end]
+    private def partition(List<Integer> values, int start, int end, Closure median) {
+        //def pivot = values[end]
+        def pivot = median(values, start, end)
         def i = start
-        for (def j = start; j <= end - 1; j++) {
-            if (values[j] < pivot) {
-                swapValues(i, j, values)
-                i++
-            }
+        def j = end
+        while (true) {
+            while (values[i] < pivot) i++
+            while (values[j] > pivot) j--
+            if (i >= j) return j
+            swapValues(i, j, values)
         }
-        swapValues(i, end, values)
-        return i
+    }
+
+    private def median_of_three(List<Integer> values, int start, int end) {
+        int mid = (int) ((start + end) / 2)
+        if (values[mid] < values[start]) swapValues(start, mid, values)
+        if (values[end] < values[start]) swapValues(start, end, values)
+        if (values[mid] < values[end]) swapValues(mid, end, values)
+        return values[end]
+    }
+
+    private def median_random(List<Integer> values, int start, int end) {
+        return values[random.nextInt(end - start + 1) + start]
+    }
+
+    private def median_first(List<Integer> values, int start, int end) {
+        return values[start]
     }
 
     private def swapValues(int a, int b, List<Integer> values) {
         int tmp = values[a]
         values[a] = values[b]
         values[b] = tmp
+    }
+
+    static void main(String[] args) {
+        println new quick_sort().run([4, 2, 9, 5, 7])
     }
 
 }
