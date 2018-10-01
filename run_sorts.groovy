a081d8a6285b3f5ea4e9cc8fe98572374af5c70e
Loic Gardaire <loic.gardaire@etu.unice.fr>
2018-10-01 13:32:00 +0200

    run all sorts on same array for each iteration

--- run_sorts.groovy
+++ run_sorts.groovy
@@ -5,43 +5,73 @@ import algorithms.quick_sort
 import utils.CSVMaker
 
 
-MAX_TWO_POW = 10
-ALGO_ITERATIONS = 1000
+MAX_TWO_POW = 5
+ALGO_ITERATIONS = 100
 
-def run_one_sort(Closure c, iterations, max_pow){
+def create_unsorted_array(int size, Random rand) {
+    def table = []
+    (1..size).each {
+        table << rand.nextInt(3 * size)
+    }
+    return table
+}
+
+def run_one_sort(Closure c, iterations, max_pow) {
     Random rand = new Random()
     def means = new HashMap()
     for (int i = 1; i <= max_pow; i++) {
         sz = 2**i
         def results = []
         for (int j = 0; j < iterations; j++) {
-            def table = []
-            (1..sz).each {
-                table << rand.nextInt(2*sz)
-            }
+            def table = create_unsorted_array(sz, rand)
             def start = System.currentTimeMillis()
             c(table)
             def now = System.currentTimeMillis()
             results << now - start
         }
-        def mean = results.sum()/results.size()
+        def mean = results.sum() / results.size()
         means.put(sz, mean)
     }
     return means
 }
 
-def run_all_sorts(){
-    def insertionSort = new insertion_sort()
-    def mergeSort = new merge_sort()
-    def heapSort = new heap_sort()
-    def quickSort = new quick_sort()
-    run_one_sort(insertionSort.&run, ALGO_ITERATIONS, MAX_TWO_POW)
-    run_one_sort(mergeSort.&run, ALGO_ITERATIONS, MAX_TWO_POW)
-    run_one_sort(heapSort.&run, ALGO_ITERATIONS, MAX_TWO_POW)
-    run_one_sort(quickSort.&run, ALGO_ITERATIONS, MAX_TWO_POW)
+
+def run_sorts(List<Closure> closures, iterations, max_pow) {
+    Random rand = new Random()
+    List<Map<Integer, Integer>> meansList = new ArrayList<>()
+    List<List<Integer>> resultsList = new ArrayList<>()
+    for (int i = 0; i < closures.size(); i++) {
+        meansList.add(new HashMap<Integer, Integer>())
+        resultsList.add([])
+    }
+    for (int i = 1; i <= max_pow; i++) {
+        sz = 2**i
+        for (int j = 0; j < iterations; j++) {
+            def table = create_unsorted_array(sz, rand)
+            for (int k = 0; k < closures.size(); k++) {
+                def c = closures.get(k)
+                def start = System.currentTimeMillis()
+                c(table)
+                def now = System.currentTimeMillis()
+                def results = resultsList.get(k)
+                results << now - start
+            }
+        }
+        for (int j = 0; j < closures.size(); j++) {
+            def results = resultsList.get(j)
+            def mean = results.sum() / results.size()
+            meansList.get(j).put(sz, mean)
+        }
+    }
+    return meansList
 }
 
 def insertionSort = new insertion_sort()
+def mergeSort = new merge_sort()
+def heapSort = new heap_sort()
+def quickSort = new quick_sort()
 def sort = run_one_sort(insertionSort.&run, ALGO_ITERATIONS, MAX_TWO_POW)
 println(sort)
 new CSVMaker(sort).generateResultsFile("results.txt")
+
+//println(run_sorts([insertionSort.&run, mergeSort.&run], ALGO_ITERATIONS, MAX_TWO_POW))
